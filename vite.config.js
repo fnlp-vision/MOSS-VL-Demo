61a4a21ccea67938d175b86683a9d666c570bc7c
henrywch <2462977089@qq.com>
2026-04-08 05:10:11 +0000

    Fixed Deployment 1.

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+export default defineConfig({
+    base: "./",
+    plugins: [vue()],
+    resolve: {
+        alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ["vue", "vue-router", "marked"],
+                },
+            },
+        },
+    },
+    server: {
+        port: 5173,
+        watch: { usePolling: true, interval: 1000 },
+    },
+});
